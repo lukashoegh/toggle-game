21ee2629dabd79a1d3004f0fa9ac85fcc5188161
Lukas Knudsen Høgh <lukas@imf.au.dk>
2017-03-22 22:29:12 +0100

    Making the color scheme consistent

--- src/TestLevel.ts
+++ src/TestLevel.ts
@@ -6,39 +6,38 @@ class TestLevel implements LevelContextualDescription {
   public size = 15;
   public parts = [
     /*{
-        type: 'trigger',
-        size: 10,
-        name: 't'
+      type: 'trigger',
+      size: 10,
+      name: 't'
     },
     {
-        input: 'turnOn'
-    },
-    {
-        type: 'indicator',
-        size: 10,
-        name: 'i'
-    },
-    {
-        type: 'button',
-        size: 10
-    },
-    {
-        to: 'i',
-        input: 'turnOff'
-    },
-    {
-        type: 'toggle',
-        size: 10,
-    },
-    {
-        to: 'i',
-        input: 'toggle'
-    },
-    {
-        to: 't',
-        input: 'release'
+      input: 'turnOn'
     },*/
     {
+      type: 'indicator',
+      size: 20,
+      name: 'i',
+      color: 'yellow'
+    },
+    {
+      type: 'button',
+      size: 20,
+      color: 'yellow'
+    },
+    {
+      to: 'i',
+      input: 'turnOff'
+    },
+    {
+      type: 'toggle',
+      size: 10,
+      color: 'yellow'
+    },
+    {
+      to: 'i',
+      input: 'toggle'
+    }
+    /*{
       type: 'spinner',
       label0: 'abc',
       label1: 'heyh',
@@ -52,7 +51,8 @@ class TestLevel implements LevelContextualDescription {
       label9: 'net',
       label10: '',
       label11: 'dc',
-    }
+      parent: 'big'
+    },*/
   ];
 }
 
